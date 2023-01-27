bf2a1b35240ec958aaf160cac34fc7c03475a772
junnumunnu2004 <121998705+junnumunnu2004@users.noreply.github.com>
2023-01-27 18:30:19 +0530

    #include<stdio.h> int main() {     int N,i,a[N],sum=0;     scanf("%d",&N);     for(i=0;i<N;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<N;i++)     {     if(i%2==0)     {
         sum=sum+a[i];
        }
        }
        printf("%d",sum);
    }

---
+++ Sum_of_Even_Positions.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int N,i,a[N],sum=0;
+    scanf("%d",&N);
+    for(i=0;i<N;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<N;i++)
+    {
+    if(i%2==0)
+    {
+    
+     sum=sum+a[i];
+    }
+    }
+    printf("%d",sum);
+}
