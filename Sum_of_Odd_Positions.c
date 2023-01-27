307bc3dbc095ee0253ac1d1cc1dec431c0e2f40c
junnumunnu2004 <121998705+junnumunnu2004@users.noreply.github.com>
2023-01-27 18:22:44 +0530

    #include<stdio.h> int main() {     int N,i,a[N],sum=0;     scanf("%d",&N);     for(i=0;i<N;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<N;i++)     {     if(i%2!=0)     {         sum=sum+a[i];     }     }     printf("%d",sum); }

---
+++ Sum_of_Odd_Positions.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int N,i,a[N],sum=0;
+    scanf("%d",&N);
+    for(i=0;i<N;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<N;i++)
+    {
+    if(i%2!=0)
+    {
+        sum=sum+a[i];
+    }
+    }
+    printf("%d",sum);
+}
